f890478c214f5ec6e3fb312361e10f9943565f22
pradipta31 <pradiptadipta31@gmail.com>
2018-02-02 23:33:31 +0800

    fix login

--- routes/web.php
+++ routes/web.php
@@ -11,10 +11,17 @@
 |
 */
 
+Route::get('/', function(){
+  return redirect('/admin');
+});;
+Route::get('/admin', function(){
+  return view('admin.index');
+});
 Route::get('/', function () {
     return view('welcome');
 });
 
-Auth::routes();
 
-Route::get('/home', 'HomeController@index')->name('home');
+Route::get('auth/login', 'Auth\LoginController@getLogin');
+Route::post('auth/login', 'Auth\LoginController@postLogin');
+Route::get('auth/logout', 'Auth\LoginController@getLogout');
