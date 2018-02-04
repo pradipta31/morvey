a04ca3a9ed58d54c47609a7530264e8598645d5b
pradipta31 <pradiptadipta31@gmail.com>
2018-02-04 15:17:07 +0800

    controller

--- app/User.php
+++ app/User.php
@@ -14,16 +14,15 @@ class User extends Authenticatable
      *
      * @var array
      */
+
+    protected $table = 'users';
+
+    
     protected $fillable = [
         'name', 'email', 'password',
     ];
 
-    /**
-     * The attributes that should be hidden for arrays.
-     *
-     * @var array
-     */
     protected $hidden = [
-        'password', 'remember_token',
+      'password', 'remember_token',
     ];
 }
--- routes/web.php
+++ routes/web.php
@@ -22,6 +22,36 @@ Route::get('/', function () {
 });
 
 
-Route::get('auth/login', 'Auth\LoginController@getLogin');
-Route::post('auth/login', 'Auth\LoginController@postLogin');
-Route::get('auth/logout', 'Auth\LoginController@getLogout');
+/*Route::group(['prefix' => 'home', 'middleware' => ['auth']], function(){
+  Route::get('/', function(){
+    $data['role'] = \App\UserRole::whereUserId(Auth::id())->get();
+    return view('home', $data);
+  });
+  Route::post('upgrade', function(Request $request){
+    if($request->ajax()){
+      $msg['success'] = 'false';
+      $user = \App\User::find($request->id);
+      if ($user)
+        $user->putRole($request->level);
+        $msg['success'] = 'true';
+
+        return response()->json($msg);
+    }
+  });
+});
+
+Route::group(['prefix' => 'admin', 'middleware' => ['auth', 'role:admin']], function(){
+  Route::get('/', function(){
+    $data['users'] = \App\User::whereDoesntHave('roles')->get();
+    return view('admin', $data);
+  });
+});*/
+
+Route::get('auth/login', 'Auth\AuthController@getLogin');
+Route::post('auth/login', 'Auth\AuthController@postLogin');
+Route::get('auth/logout', 'Auth\AuthController@getLogout');
+
+Route::get('auth/register', 'Auth\AuthController@getRegister');
+Route::post('auth/register', 'Auth\AuthController@postRegister');
+
+Auth::routes();
